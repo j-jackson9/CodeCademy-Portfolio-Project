190ea40d68e77a1d6aac00fc260f01528e2ed626
j-jackson9 <jordancjack.20@gmail.com>
2024-05-18 12:10:23 -0500

    style nav links and update website

---
+++ styles.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let homeLink = document.querySelector('.navbar .home');
+    let contactLink = document.querySelector('.navbar .contact');
+
+    /* Home link */
+    function changeHomeColor() {
+        homeLink.style.color = 'red';
+    }
+    function resetHomeColor() {
+        homeLink.style.color = 'white';
+    }
+    homeLink.addEventListener('mouseover', changeHomeColor);
+    homeLink.addEventListener('mouseout', resetHomeColor);
+
+    /* Contact link */
+    function changeContactColor() {
+        contactLink.style.color = 'red';
+    }
+    function resetContactColor() {
+        contactLink.style.color = 'white';
+    }
+    contactLink.addEventListener('mouseover', changeContactColor);
+    contactLink.addEventListener('mouseout', resetContactColor);
+});
