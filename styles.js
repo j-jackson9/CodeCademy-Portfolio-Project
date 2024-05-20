5649840068fd489ed838f90555d3f4af3636e2f1
j-jackson9 <jordancjack.20@gmail.com>
2024-05-20 18:15:28 -0500

    add glow effect to skill icons/nav links on home page, start project section on home page, start working on contact page

--- styles.js
+++ styles.js
@@ -8,8 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
     function resetHomeColor() {
         homeLink.style.color = 'white';
     }
+    function increaseHomeSize() {
+        homeLink.style.fontSize = '30px';
+    }
+    function resetHomeSize() {
+        homeLink.style.fontSize = '20px';
+    }
     homeLink.addEventListener('mouseover', changeHomeColor);
     homeLink.addEventListener('mouseout', resetHomeColor);
+    homeLink.addEventListener('mouseover', increaseHomeSize);
+    homeLink.addEventListener('mouseout', resetHomeSize);
+    
 
     /* Contact link */
     function changeContactColor() {
@@ -18,7 +27,16 @@ document.addEventListener('DOMContentLoaded', () => {
     function resetContactColor() {
         contactLink.style.color = 'white';
     }
+    function increaseContactSize() {
+        contactLink.style.fontSize = '30px';
+    }
+    function resetContactSize() {
+        contactLink.style.fontSize = '20px';
+    }
     contactLink.addEventListener('mouseover', changeContactColor);
     contactLink.addEventListener('mouseout', resetContactColor);
+    contactLink.addEventListener('mouseover', increaseContactSize);
+    contactLink.addEventListener('mouseout', resetContactSize);
+
 
 });
